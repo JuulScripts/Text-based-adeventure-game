60706e5f35e4fdf051dec9bfaf36832165a8482b
JuulScripts <jscripts11@gmail.com>
2025-05-21 11:34:12 +0200

    Made item class

--- Text based/Item.cs
+++ Text based/Item.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Text_based
 {
     internal class Item
     {
+        string Name { get; set; }
+        int Value { get; set; }
+        bool IsDangerous {  get; set; }
 
+
+        public Item(string name, int value, bool isDangerous)
+        {
+            Name = name;
+            Value = value;
+            IsDangerous = isDangerous;
+        }
+
+        public static void AddItemToInventory(Item item)
+        {
+
+        }
     }
 }
